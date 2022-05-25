e985804b4562ece42d3be970162e37044a671afc
Kevin Murphy <kevin@threatswitch.com>
2022-05-25 00:20:23 -0400

    task: nav updates

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -7,12 +7,23 @@ const Navigation = () => (
   <nav role="navigation" className={styles.container} aria-label="Main">
     <Link to="/" className={styles.logoLink}>
       <span className={styles.logo} />
-      <span className={styles.navigationItem}>Gatsby Starter Contentful</span>
+      <span className={styles.navigationItem}>Atlantis Granite and Marble</span>
     </Link>
+
     <ul className={styles.navigation}>
       <li className={styles.navigationItem}>
         <Link to="/" activeClassName="active">
           Home
+        </Link>
+      </li>
+      <li className={styles.navigationItem}>
+        <Link to="/contact/" activeClassName="active">
+          Contact
+        </Link>
+      </li>
+      <li className={styles.navigationItem}>
+        <Link to="/reviews/" activeClassName="active">
+          Reviews
         </Link>
       </li>
       <li className={styles.navigationItem}>
