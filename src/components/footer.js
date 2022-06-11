9b28ebfa81f65e75b45b3fac6fdeb4c5c8f3dcfc
Kevin Murphy <kevin@threatswitch.com>
2022-06-11 16:36:43 -0400

    task: footer data

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,9 +6,41 @@ import * as styles from './footer.module.css'
 const Footer = () => (
   <Container as="footer">
     <div className={styles.container}>
-      Built with <a href="https://contentful.com/">Contentful</a> and{' '}
-      <a href="https://gatsbyjs.com">Gatsby</a> &middot;{' '}
-      <a href="https://github.com/contentful/starter-gatsby-blog">Source</a>
+      <div className="v-card">
+        <div className="fn org">Atlantis Granite &amp; Marble LLC</div>
+        <div className="adr">
+          <span className="street-address">3280 Peachtree Corners Cir B</span>
+          <br />
+          <span className="locality">Peachtree Corners</span>,&nbsp;
+          <span className="region">GA</span>&nbsp;
+          <span className="postal-code">30092</span>
+        </div>
+        <a
+          className="phonelink tel"
+          // onclick="ga('send', 'event', 'Phone Call Click', 'click', 'Footer (678) 292-6600');"
+          href="tel:+16782713409"
+        >
+          <span className="icon-phone"></span>(678) 271-3409
+        </a>
+      </div>
+      <div className="socialbuttonspanel">
+        <a
+          className="socialbuttonslink"
+          // onclick="ga('send', 'event', 'Social Media Click', 'click', 'https://www.facebook.com/atlantisgranite/');"
+          href="https://www.facebook.com/atlantisgranite/"
+          target="_blank"
+        >
+          <span className="icon-facebook-round"></span>
+        </a>
+      </div>
+      <div className="footercredit">
+        <span>
+          Copyright © 2022 Atlantis Granite &amp; Marble LLC, All rights
+          reserved.
+        </span>
+      </div>
+      <p>Privacy Policy</p>
+      <p>Terms Of Service</p>
     </div>
   </Container>
 )
